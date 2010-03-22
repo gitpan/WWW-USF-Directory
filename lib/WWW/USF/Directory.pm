a17eeea7b640ae357ba3af1b1da2052f5a941522
Douglas Christopher Wilson <doug@somethingdoug.com>
2010-03-22 00:53:46 +0000

    Import of DOUGDUDE/WWW-USF-Directory-0.002_002 from CPAN.
    gitpan-cpan-distribution: WWW-USF-Directory
    gitpan-cpan-version:      0.002_002
    gitpan-cpan-path:         DOUGDUDE/WWW-USF-Directory-0.002_002.tar.gz
    gitpan-cpan-author:       DOUGDUDE
    gitpan-cpan-maturity:     developer

--- lib/WWW/USF/Directory.pm
+++ lib/WWW/USF/Directory.pm
@@ -7,7 +7,7 @@ use warnings 'all';
 ###########################################################################
 # METADATA
 our $AUTHORITY = 'cpan:DOUGDUDE';
-our $VERSION   = '0.002_001';
+our $VERSION   = '0.002_002';
 
 ###########################################################################
 # MOOSE
@@ -30,7 +30,6 @@ use HTML::HTML5::Parser 0.03;
 use List::MoreUtils 0.07;
 use Net::SAJAX 0.102;
 use Readonly 1.03;
-use URI;
 use WWW::USF::Directory::Entry;
 use WWW::USF::Directory::Entry::Affiliation;
 use WWW::USF::Directory::Exception;
@@ -173,6 +172,7 @@ sub search {
 
 	# Get the advanced search parameters
 	my ($campus, $college, $department) =
+		map { length($_) ? $_ : $args{$_} } # Restore to original if it didn't exist
 		map { $self->_advanced_search_parameter_id($_ => $args{$_}) }
 		qw(campus college department);
 
@@ -313,7 +313,7 @@ sub _build_sajax {
 
 	# This will return a SAJAX object with default options
 	return Net::SAJAX->new(
-		url => URI->new($self->directory_url->clone),
+		url => $self->directory_url->clone,
 	);
 }
 
@@ -328,17 +328,20 @@ sub _clean_node_text {
 	# Find all the line breaks
 	foreach my $br ($node->getElementsByTagName('br')) {
 		# Replace the line breaks with a text node with a new line
-		$br->replaceNode($node->ownerDocument->createTextNode("\n"));
+		$br->replaceNode($node->ownerDocument->createTextNode('{NEWLINE}'));
 	}
 
 	# Get the text of the node (make sure it is native UTF-8)
 	my $text = Encode::encode_utf8($node->textContent);
 
 	# Transform all the horizontal space into ASCII spaces
-	$text =~ s{\h+}{ }gmsx;
+	$text =~ s{\s+}{ }gmsx;
 
 	# Truncate leading and trailing horizontal space
-	$text =~ s{^\h+|\h+$}{}gmsx;
+	$text =~ s{^\s+|\s+$}{}gmsx;
+
+	# Change the new-lines back
+	$text =~ s{{NEWLINE}}{\n}gmsx; # Because perl < 5.10 cannot do \v and \h
 
 	# Return the text
 	return $text;
@@ -464,7 +467,7 @@ sub _table_row_to_entry {
 
 	if (exists $row{given_name}) {
 		# Split on vertical whitespace
-		my @given_names = split m{\v+}msx, $row{given_name};
+		my @given_names = split m{[\r\n]+}msx, $row{given_name};
 
 		# The first two given names are as follows
 		my ($first_name, $middle_name) = @given_names;
@@ -482,7 +485,7 @@ sub _table_row_to_entry {
 
 	if (exists $row{affiliation}) {
 		# There could be zero or more affiliations seperated by vertical space
-		my @affiliations = split m{\h*\v+\h*}msx, delete $row{affiliation};
+		my @affiliations = split m{\s*[\r\n]+\s*}msx, delete $row{affiliation};
 
 		# Change the affiliation to objects
 		foreach my $affiliation (@affiliations) {
@@ -497,7 +500,7 @@ sub _table_row_to_entry {
 	foreach my $value (values %row) {
 		if (ref $value eq q{}) {
 			# A string, so remove vertical whitespace
-			$value =~ s{\h*\v+\h*}{ }gmsx;
+			$value =~ s{\s*[\r\n]+\s*}{ }gmsx;
 		}
 	}
 
@@ -533,7 +536,7 @@ WWW::USF::Directory - Access to USF's online directory
 
 =head1 VERSION
 
-Version 0.002_001
+Version 0.002_002
 
 =head1 SYNOPSIS
 
